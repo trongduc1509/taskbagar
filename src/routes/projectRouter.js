89875aa0b58d4df40e0d66d6aab79bd2443fab1b
trongduc1509 <trongduc1509@gmail.com>
2022-05-22 21:13:03 +0700

    fix some bugs

--- src/routes/projectRouter.js
+++ src/routes/projectRouter.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/projectController');
-router.get("/", controller.detailProject);
+//router.get("/", controller.detailProject);
 router.get('/', controller.list);
 router.get("/employeeProject", controller.employeeProject);
 router.get("/:name", controller.searchProject);
-
 
 module.exports = router;
