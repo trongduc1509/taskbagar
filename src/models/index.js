b9d8c18d74effce0d436f1d6121f71c7f0f61329
tranmanhduc2711 <manhduc2711@gmail.com>
2022-05-27 14:19:11 +0700

    fix port

--- src/models/index.js
+++ src/models/index.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "postgres",
     //default port = 5432, manhduc's port =5000
-    port: 5000,
+    port: process.env.DB_PORT,
   }
 );
 
